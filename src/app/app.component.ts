cd3f3b95b495df2695fa1bb6a9bcfdbf620c0ff6
Juan Jose Toro <juanjosetoro09@gmail.com>
2024-06-16 21:38:46 -0400

    Added frontend functionality to fetch files from the backend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,42 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, Injectable, Input, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs';
 import { RouterOutlet } from '@angular/router';
+import markdownit from 'markdown-it';
+
+
+@Injectable({ providedIn: 'root' })
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   template: `
-    <div class="max-w-screen-md mx-auto">
-      Testing this
+    <div [innerHTML]="pageContent" class="max-w-screen-md mx-auto">
     </div>
   `,
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'blog testing again';
+  private md;
+  private apiUrl = 'http://localhost:3000';
+  @Input() pageContent: any;
+
+  constructor(private http: HttpClient) {
+    this.md = markdownit();
+  }
+
+  getPage(): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/`);
+  }
+
+  ngOnInit() {
+    this.getPage().subscribe({
+      next: page => {
+        console.log('page:', page)
+        this.pageContent = this.md.render(page.result);
+      },
+    })
+  }
 }
